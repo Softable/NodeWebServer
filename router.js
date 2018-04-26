8d941178122b1f1ba8b47e451ac9ec60058c2307
AlMax <alihaider.maqsood@gmail.com>
2018-04-26 09:26:40 +0200

    Router directory access

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
 module.exports = function(app)
 {
     app.get('/', function(req,res) {
-        res.render('index.html');
+        res.render('Utenti/index.html');
     });
     app.get('/inserisci', function(req,res) {
-        res.render('inserisci.html');
+        res.render('Utenti/inserisci.html');
     });
 }
