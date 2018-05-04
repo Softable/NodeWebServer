1e65c413d50090a5bfbfc54151b6640e4967a330
AlMax <alihaider.maqsood@gmail.com>
2018-05-04 19:30:57 +0200

    Router
    Sistemato il router

--- router.js
+++ router.js
@@ -1,9 +1,50 @@
 module.exports = function(app)
 {
-    app.get('/', function(req,res) {
-        res.render('Utenti/index.html');
-    });
-    app.get('/inserisci', function(req,res) {
-        res.render('Utenti/inserisci.html');
-    });
+  app.get('/', function(req,res) {
+      res.render('index.html');
+  });
+
+  app.get('/modello/collezione', function(req,res) {
+      res.render('modello/collezione.html');
+  });
+
+  app.get('/modello/inserisci', function(req,res) {
+      res.render('modello/inserisci.html');
+  });
+
+  app.get('/modello/richiedi', function(req,res) {
+      res.render('modello/richiedi.html');
+  });
+
+  app.get('/modello/elimina', function(req,res) {
+      res.render('modello/elimina.html');
+  });
+
+  app.get('/modello/modifica', function(req,res) {
+      res.render('modello/modifica.html');
+  });
+
+  app.get('/utente/collezione', function(req,res) {
+      res.render('utente/collezione.html');
+  });
+
+  app.get('/utente/inserisci', function(req,res) {
+      res.render('utente/inserisci.html');
+  });
+
+  app.get('/utente/richiedi', function(req,res) {
+      res.render('utente/richiedi.html');
+  });
+
+  app.get('/utente/elimina', function(req,res) {
+      res.render('utente/elimina.html');
+  });
+
+  app.get('/utente/modifica', function(req,res) {
+      res.render('utente/modifica.html');
+  });
+
+  app.get('/tabella', function(req,res) {
+      res.render('tabella.html');
+  });
 }
