c9f4570f8be274e9d85895ffde93c642474b509b
AlMax <alihaider.maqsood@gmail.com>
2018-05-04 22:11:06 +0200

    Creata directory tabella
    Directory tabella, in views, con alcune sue pagine

--- router.js
+++ router.js
@@ -4,6 +4,26 @@ module.exports = function(app)
       res.render('index.html');
   });
 
+  app.get('/utente/collezione', function(req,res) {
+      res.render('utente/collezione.html');
+  });
+
+  app.get('/utente/inserisci', function(req,res) {
+      res.render('utente/inserisci.html');
+  });
+
+  app.get('/utente/richiedi', function(req,res) {
+      res.render('utente/richiedi.html');
+  });
+
+  app.get('/utente/elimina', function(req,res) {
+      res.render('utente/elimina.html');
+  });
+
+  app.get('/utente/modifica', function(req,res) {
+      res.render('utente/modifica.html');
+  });
+
   app.get('/modello/collezione', function(req,res) {
       res.render('modello/collezione.html');
   });
@@ -24,24 +44,24 @@ module.exports = function(app)
       res.render('modello/modifica.html');
   });
 
-  app.get('/utente/collezione', function(req,res) {
-      res.render('utente/collezione.html');
+  app.get('/tabella/collezione', function(req,res) {
+      res.render('tabella/collezione.html');
   });
 
-  app.get('/utente/inserisci', function(req,res) {
-      res.render('utente/inserisci.html');
+  app.get('/tabella/inserisci', function(req,res) {
+      res.render('tabella/inserisci.html');
   });
 
-  app.get('/utente/richiedi', function(req,res) {
-      res.render('utente/richiedi.html');
+  app.get('/tabella/richiedi', function(req,res) {
+      res.render('tabella/richiedi.html');
   });
 
-  app.get('/utente/elimina', function(req,res) {
-      res.render('utente/elimina.html');
+  app.get('/tabella/elimina', function(req,res) {
+      res.render('tabella/elimina.html');
   });
 
-  app.get('/utente/modifica', function(req,res) {
-      res.render('utente/modifica.html');
+  app.get('/tabella/modifica', function(req,res) {
+      res.render('tabella/modifica.html');
   });
 
   app.get('/tabella', function(req,res) {
---
+++ views/js/tabella.js
@@ -0,0 +1,50 @@
+function visualizza_collezione() {
+    $(document).ready(function() {
+        $("#tabella").ready(function( event ) {
+            $.get("http://localhost:8080/tabella/collezione", function(data) {
+                $(function(){
+                    var jsonObj = data;
+                    var table = "";
+                    $.each(jsonObj, function(key, value){
+                        table += "<tr>";
+                        table += "<td>" + key + "</td>";
+                        table += "<td>" + $(value).attr("id_tabella") + "</td>";
+                        table += "<td>" + $(value).attr("username_utente") + "</td>";
+                        table += "<td>" + $(value).attr("id_modelloTabella") + "</td>";
+                        table += "<td><select>";
+                        for (var i = 0; i < $(value).attr("tabella").length; i++)
+                            table += "<option>" + $(value).attr("tabella")[i] + "</option>";
+                        table += "</select></td>";
+                        table += "</tr>";
+                    });
+                    $("#tabella").append(table);
+                });
+            });
+        });
+    });
+}
+
+function richiedi_modello(){
+    $(document).ready(function() {
+        var id_modello = document.getElementById("id_tabella").value;
+        $("#tabella").ready(function( event ) {
+            $.get("http://localhost:8080/tabella/richiedi/"+id_modello, function(data) {
+                $(function(){
+                    var jsonObj = data;
+                    var table = "";
+                    table += "<tr>";
+                    table += "<td>" + key + "</td>";
+                    table += "<td>" + $(value).attr("id_tabella") + "</td>";
+                    table += "<td>" + $(value).attr("username_utente") + "</td>";
+                    table += "<td>" + $(value).attr("id_modelloTabella") + "</td>";
+                    table += "<td><select>";
+                    for (var i = 0; i < $(value).attr("tabella").length; i++)
+                        table += "<option>" + $(value).attr("tabella")[i] + "</option>";
+                    table += "</select></td>";
+                    table += "</tr>";
+                    document.getElementById("tabella").innerHTML+=table;
+                });
+            });
+        });
+    });
+}
