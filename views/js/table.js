d0d3d8cf52f6df588d3331de8ed81ba8e2ab336c
AlMax <alihaider.maqsood@gmail.com>
2018-05-17 11:48:05 +0200

    Inserimento Tabella in pagina
    Completata sia la parte Js che Html che Css riguardo l'inseirmento di tabelle in pagina, verificando pure che siano presenti univocamente.

--- views/js/table.js
+++ views/js/table.js
@@ -1,23 +1,31 @@
-function visualizza_tabella(id){
-	var tabella=richiedi_tabella(id);
-	var quantita_dati=tabella.valori.length;
-	var id_modello=tabella.id_modelloTabella;
-	var modello=richiedi_modello(id_modello);
-	var colonne=modello.attributi.length;
-	var tabellaHTML = document.getElementById('tabella').getElementsByTagName('tbody')[0];
-	for(var i=0 ; i<colonne ; i++){
-    document.getElementById('attributi').innerHTML+="<th class='column100 column"+(i+1)+"' data-column='column"+(i+1)+"'>"+modello.attributi[i]+"</th>";
-	}
-	for(var i=0 ; i<quantita_dati ; i++){
-		if(i%colonne==0){
-			var riga = tabellaHTML.insertRow(tabellaHTML.rows.length);
+function inserisci_tabella(id){
+	if (document.getElementById("tabella"+id)) {
+    alert("La tabella esiste già!");
+	}else{
+		var tabella=richiedi_tabella(id);
+		var quantita_dati=tabella.valori.length;
+		var id_modello=tabella.id_modelloTabella;
+		var modello=richiedi_modello(id_modello);
+		var colonne=modello.attributi.length;
+		$("#div_table").append("<table data-vertable='ver1' id='tabella"+id+"'><thead><tr class='row100 head' id='attributi"+id+"'></tr></thead><tbody id='dati"+id+"'></tbody></table>");
+		var tabellaHTML = document.getElementById("tabella"+id).getElementsByTagName("tbody")[0];
+		for(var i=0 ; i<colonne ; i++){
+	    document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(i+1)+"' data-column='column"+(i+1)+"'>"+modello.attributi[i]+"</th>";
 		}
-		var cella = riga.insertCell(i%3);
-		var dato  = tabella.valori[i];
-		cella.innerHTML+=(dato);
+		for(var i=0 ; i<quantita_dati ; i++){
+			if(i%colonne==0){
+				var riga = tabellaHTML.insertRow(tabellaHTML.rows.length);
+			}
+			var cella = riga.insertCell(i%colonne);
+			cella.id="cella"+id+((i)+1);
+			var dato  = tabella.valori[i];
+			cella.innerHTML+=(dato);
+			document.getElementById("cella"+id+((i)+1)).setAttribute("class", "column100 column"+((i%colonne)+1));
+			document.getElementById("cella"+id+((i)+1)).setAttribute("data-column", "column"+((i%colonne)+1));
+			document.getElementById("cella"+id+((i)+1)).setAttribute("style", "text-align:center;");
+		}
+		jQuery("tr").addClass("row100");
 	}
-	jQuery("tr").addClass("row100");
-	jQuery("td").addClass("column100");
 }
 
 function richiedi_tabella(id_tabella){
@@ -63,7 +71,7 @@ function change_color(version) {
 		var table2 = $(this).parent().parent();
 		var verTable = $(table1).data('vertable') + "";
 		var column = $(this).data('column') + "";
-		
+
 		$(table2).find("." + column).addClass('hov-column-' + verTable);
 		$(table1).find(".row100.head ." + column).addClass('hov-column-head-' + verTable);
 	});
