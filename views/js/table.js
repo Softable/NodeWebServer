12e10720a0aab52335c0cf3d19e6a1188548879e
AlMax <alihaider.maqsood@gmail.com>
2018-05-31 11:52:09 +0200

    Modifica riga
    Completata la funzione del bottone modifica riga, che modifica pure i dati nel DB e fa un refresh della pagina per essere sicuri della presenza delle nuove modifiche.

--- views/js/table.js
+++ views/js/table.js
@@ -21,24 +21,32 @@ function inserisci_tabella_inPagina(id){
 			var colonne=modello.attributi.length;
 			$("#div_table").append("<table data-vertable='ver1' id='tabella"+id+"'><thead><tr class='row100 head' id='attributi"+id+"'></tr></thead><tbody id='dati"+id+"'></tbody></table>");
 			var tabellaHTML = document.getElementById("tabella"+id).getElementsByTagName("tbody")[0];
-			document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(colonne+1)+"' data-column='column"+(colonne+1)+"'><button onclick='elimina_tabella(this.value)' value='"+id+"'>Elimina la Tabella</button></th>";
+			document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(colonne+1)+"' data-column='column"+(colonne+1)+"'>Modifica</th>";
+			document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(colonne+2)+"' data-column='column"+(colonne+2)+"'><button onclick='elimina_tabella(this.value)' value='"+id+"'>Elimina la Tabella</button></th>";
 			for(var i=0 ; i<colonne ; i++){
-		    document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(i+1)+"' data-column='column"+(i+1)+"'>"+modello.attributi[i]+"</th>";
+		    document.getElementById('attributi'+id).innerHTML+="<th class='column100 column"+(i+2)+"' data-column='column"+(i+2)+"'>"+modello.attributi[i]+"</th>";
 			}
 			for(var i=0,j=0; i<quantita_dati ; i++){
 				if(i%colonne==0){
 					var riga = tabellaHTML.insertRow(tabellaHTML.rows.length);
 					riga.id = "riga"+id+""+j;
-					var cella = riga.insertCell(i%colonne);
+					var cella = riga.insertCell(0);
+					cella.id="cella"+id+"M"+((i)+1);
+					var bottone_modifica = "<button onclick='modifica_riga(this.value)' value='"+"riga"+id+""+j+"'>Modifica la Riga</button>";
+					cella.innerHTML+=(bottone_modifica);
+					cella = riga.insertCell(1);
 					cella.id="cella"+id+"E"+((i)+1);
-					var dato = "<button onclick='elimina_riga(this.value)' value='"+"riga"+id+""+j+"'>Elimina la Riga</button>";
-					cella.innerHTML+=(dato);
-					document.getElementById("cella"+id+"E"+((i)+1)).setAttribute("class", "column100 column"+(colonne+1));
-					document.getElementById("cella"+id+"E"+((i)+1)).setAttribute("data-column", "column"+(colonne+1));
+					var bottone_elimina = "<button onclick='elimina_riga(this.value)' value='"+"riga"+id+""+j+"'>Elimina la Riga</button>";
+					cella.innerHTML+=(bottone_elimina);
+					document.getElementById("cella"+id+"M"+((i)+1)).setAttribute("class", "column100 column"+(colonne+1));
+					document.getElementById("cella"+id+"M"+((i)+1)).setAttribute("data-column", "column"+(colonne+1));
+					document.getElementById("cella"+id+"M"+((i)+1)).setAttribute("style", "text-align:center;");
+					document.getElementById("cella"+id+"E"+((i)+1)).setAttribute("class", "column100 column"+(colonne+2));
+					document.getElementById("cella"+id+"E"+((i)+1)).setAttribute("data-column", "column"+(colonne+2));
 					document.getElementById("cella"+id+"E"+((i)+1)).setAttribute("style", "text-align:center;");
 					j++;
 				}
-				var cella = riga.insertCell((i%colonne)+1);
+				var cella = riga.insertCell((i%colonne)+2);
 				cella.id="cella"+id+((i)+1);
 				var dato = tabella.valori[i];
 				cella.innerHTML+=(dato);
@@ -68,28 +76,64 @@ function leggiCookie(name) {
     return 'null';
 }
 
+function modifica_riga(id_riga){
+	var tabella_elementi = idTabella_posizioniElementi(id_riga);
+	var posizioni_dati = tabella_elementi[1];
+	var celle = document.getElementById(id_riga).cells;
+	var cella_modifica = celle[0];
+	cella_modifica.innerHTML = "<button onclick=conferma_modifiche('"+id_riga+"') value='"+cella_modifica.id+"'>Esegui le modifiche</button>";
+	for (var i=2; i<celle.length ; i++){
+		celle[i].innerHTML = "<input id='"+celle[i].id+"in' value='"+celle[i].innerHTML+"'></input>";
+	}
+}
+
+function conferma_modifiche(id_riga){
+	var tabella_elementi = idTabella_posizioniElementi(id_riga);
+	var id_tabella = tabella_elementi[0];
+	var posizioni_dati = tabella_elementi[1];
+	var tabella = richiedi_tabella(id_tabella);
+	var dati = tabella.valori;
+	var celle = document.getElementById(id_riga).cells;
+	var cella_modifica = celle[0];
+	for (var i=2,j=0; i<celle.length ; i++,j++){
+		dati[posizioni_dati[j]-1] = document.getElementById(celle[i].id+"in").value;
+	}
+	modifica_tabella(id_tabella,tabella.username_utente,tabella.id_modelloTabella,dati);
+	location.reload();
+}
+
 function elimina_riga(id_riga){
 	if(confirm("Sei sicuro di voler eliminare la riga?")){
-		var riga = document.getElementById(id_riga);
-		var celle = document.getElementById(id_riga).cells;
-		var posizione_cella = [];
-		var posizione_riga = id_riga.split("riga");
-		var posizione_cella_inTabella;
-		var posizioni_dati = [];
-		for (var i=1; i<celle.length ; i++){
-			posizione_cella = celle[i].id.split("cella");
-			posizione_cella_inTabella = posizione_cella[1]-(Math.floor(posizione_riga[1]/10)*10);
-			posizioni_dati.push(posizione_cella_inTabella);
-		}
-		var id_tabella = (posizione_riga[1]/((posizione_cella_inTabella.toString().length)*10)).toFixed(0);
+		var tabella_elementi = idTabella_posizioniElementi(id_riga);
+		var id_tabella = tabella_elementi[0];
+		var posizioni_dati = tabella_elementi[1];
 		var tabella = richiedi_tabella(id_tabella);
 		var dati = tabella.valori;
 		for(var posizione=posizioni_dati.length-1 ; posizione>=0 ; posizione--){
     	dati.splice(posizioni_dati[posizione]-1, 1);
 		}
 		modifica_tabella(id_tabella,tabella.username_utente,tabella.id_modelloTabella,dati);
+		var riga = document.getElementById(id_riga);
 		riga.parentNode.removeChild(riga);
 	}
+}
+
+function idTabella_posizioniElementi(id_riga){
+	var celle = document.getElementById(id_riga).cells;
+	var posizione_cella = [];
+	var posizione_riga = id_riga.split("riga");
+	var posizione_cella_inTabella;
+	var posizioni_dati = [];
+	for (var i=2; i<celle.length ; i++){
+		posizione_cella = celle[i].id.split("cella");
+		posizione_cella_inTabella = posizione_cella[1]-(Math.floor(posizione_riga[1]/10)*10);
+		posizioni_dati.push(posizione_cella_inTabella);
+	}
+	var id_e_elementi = [];
+	var id_tabella = (posizione_riga[1]/((posizione_cella_inTabella.toString().length)*10)).toFixed(0);
+	id_e_elementi.push(id_tabella);
+	id_e_elementi.push(posizioni_dati);
+	return id_e_elementi;
 }
 
 function elimina_tabella(id_tabella) {
@@ -103,6 +147,8 @@ function elimina_tabella(id_tabella) {
           console.log("Esito eliminazione: "+esito);
         });
     });
+		var tabella = document.getElementById("tabella"+id_tabella);
+		tabella.parentNode.removeChild(tabella);
 	}
 }
 
