6d096dfa6143214511894ee28f39fa8c08042ca5
AlMax <alihaider.maqsood@gmail.com>
2018-06-12 00:49:14 +0200

    Revert "Merge branch 'master' of https://github.com/Softable/NodeWebServer"
    This reverts commit 4f096b757e99eccd20864354998d6425b405435c, reversing
    changes made to 9f119fc4f00dc8ff36d793357cf8a610db3a1e51.

--- views/js/utente.js
+++ views/js/utente.js
@@ -1,116 +1,114 @@
 /* VISUALIZZAZIONE DI TUTTI GLI UTENTI DAL DATABASE */
 function visualizza_utenti(){
-	var utenti = [];
-	$.ajax({
-		url: 'http://localhost:8080/utente/collezione',
-		type: 'GET',
-		async: false,
-		success: function(data) {
-			data.forEach(
-				function(utente) {
-					utenti.push(utente);
-				}
-			);
+  var utenti = [];
+  $.ajax({
+    url: 'http://localhost:8080/utente/collezione',
+    type: 'GET',
+    async: false,
+    success: function(data) {
+      data.forEach(
+		function(utente) {
+			utenti.push(utente);
 		}
-	});
-	return utenti;
+	  );
+	}
+  });
+  return utenti;
 }
 
 /* RICHIESTA DI UN UTENTE */
 function richiedi_utente(username) {
-	var utente = {};
-		$.ajax({
-			url: 'http://localhost:8080/utente/richiedi/' + String(username),
-			type: 'GET',
-			async: false,
-			success: function(data) {
-				utente.username = username;
-				utente.nome = data.nome;
-				utente.cognome = data.cognome;
-				utente.email = data.email;
-				utente.password = data.password;
-			}
-		});
-	return utente;
+  var utente = {};
+  $.ajax({
+    url: 'http://localhost:8080/utente/richiedi/' + String(username),
+    type: 'GET',
+    async: false,
+    success: function(data) {
+      utente.username = username;
+      utente.nome = data.nome;
+      utente.cognome = data.cognome;
+      utente.email = data.email;
+      utente.password = data.password;
+    }
+  });
+  return utente;
 }
 
 
 function inserisci_utente() {
-	$(document).ready(function() {
-		var utente = {}, confPassword;
-		utente.username = document.getElementById('reg_username').value;
-		utente.nome = document.getElementById('nome').value;
-		utente.cognome = document.getElementById('cognome').value;
-		utente.email = document.getElementById('email').value;
-		utente.password = document.getElementById('reg_password').value;
-		confPassword = document.getElementById('conf_password').value;
-		if (confPassword == utente.password) {
-			$.ajax({
-				url: 'http://localhost:8080/utente/inserisci',
-				type: 'POST',
-				contentType: 'application/json',
-				data: JSON.stringify(utente),
-				dataType: 'json'
-			}).done(function (esito) {
-				if (esito) {
-					creaCookie('username', document.getElementById('reg_username').value, 10);
-					window.location.replace("/");
-				} else {
-					window.location.replace("/error?type=1");
-				}
-			});
+  $(document).ready(function() {
+    var utente = {}, confPassword;
+    utente.username = document.getElementById('reg_username').value;
+    utente.nome = document.getElementById('nome').value;
+    utente.cognome = document.getElementById('cognome').value;
+    utente.email = document.getElementById('email').value;
+    utente.password = document.getElementById('reg_password').value;
+    confPassword = document.getElementById('conf_password').value;
+    if (confPassword == utente.password) {
+      $.ajax({
+        url: 'http://localhost:8080/utente/inserisci',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(utente),
+        dataType: 'json'
+      }).done(function (esito) {
+        if (esito) {
+          window.location.replace("/");
 		} else {
-			window.location.replace("/error?type=2");
+          window.location.replace("/error?type=1");
 		}
-	});
+      });
+    } else {
+      window.location.replace("/error?type=2");
+	}
+  });
 }
 
 function modifica_utente() {
-	$(document).ready(function() {
-		var obj = {};
-		obj.username = document.getElementById('usernameMod').value;
-		obj.nome = document.getElementById('nomeMod').value;
-		obj.cognome = document.getElementById('cognomeMod').value;
-		obj.email = document.getElementById('emailMod').value;
-		obj.password = document.getElementById('passMod').value;
-		$.ajax({
-			url: 'http://localhost:8080/utente/modifica',
-			type: 'PUT',
-			contentType: 'application/json',
-			data: JSON.stringify(obj),
-			dataType: 'json'
-		}).done(function (esito) {
-			alert("Esito modifica: "+ esito);
-		});
-	});
+  $(document).ready(function() {
+    var obj = {};
+    obj.username = document.getElementById('usernameMod').value;
+    obj.nome = document.getElementById('nomeMod').value;
+    obj.cognome = document.getElementById('cognomeMod').value;
+    obj.email = document.getElementById('emailMod').value;
+    obj.password = document.getElementById('passMod').value;
+    $.ajax({
+        url: 'http://localhost:8080/utente/modifica',
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(obj),
+        dataType: 'json'
+    }).done(function (esito) {
+      alert("Esito modifica: "+ esito);
+    });
+  });
 }
 
 function elimina_utente() {
-	$(document).ready(function() {
-		var obj = "/" + String(document.getElementById('usernameDel').value);
-		$.ajax({
-			url: 'http://localhost:8080/utente/elimina' + obj,
-			type: 'GET',
-		}).done(function (esito) {
-			alert("Esito eliminazione: " + esito);
-		});
-	});
+    $(document).ready(function() {
+      var obj = "/" + String(document.getElementById('usernameDel').value);
+      $.ajax({
+      url: 'http://localhost:8080/utente/elimina' + obj,
+      type: 'GET',
+      }).done(function (esito) {
+        alert("Esito eliminazione: " + esito);
+      });
+    });
 }
 
 function accedi_utente() {
-	var username, password, utente;
+  var username, password, utente;
 
-	username = document.getElementById("username").value;
-	password = document.getElementById("password").value;
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
 
-	utente = richiedi_utente(username);
-
-	if (utente.password == password) {
-		creaCookie('username', document.getElementById('username').value, 10);  
-		window.location.replace("/");
-	} else {
-		window.location.replace("/error");
-	}
+  utente = richiedi_utente(username);
+  
+  if (utente.password == password) {
+    window.location.replace("/");
+  } else {
+    window.location.replace("/error");
+  }
 }
 
 function creaCookie(name,value) {
@@ -131,11 +129,5 @@ function leggiCookie(name) {
 	}
 	return 'null';
 }	
-
-function esci() {
-	document.cookie = "username=null;";
-	window.location("../");
-}
-
 
 
