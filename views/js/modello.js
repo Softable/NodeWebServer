5abaa984695d56b8097ab8f6cdbc2a1f001aac57
Axel769 <sgaro99@gmail.com>
2018-06-15 14:17:24 +0200

    Aggiunta della possibilità di modificare i modelli

--- views/js/modello.js
+++ views/js/modello.js
@@ -38,6 +38,7 @@ function richiedi_modello(id_modello){
 		success: function(data) {
 			modello.id_modello = id_modello;
 			modello.nome_modello = data.nome_modello;
+			modello.username = data.username;
 			modello.attributi = data.attributi;
 		}
 	});
@@ -70,30 +71,28 @@ function inserisci_modello() {
 	});
 }
 
-function modifica_modello() {
-	$(document).ready(function() {
-		var modello = {}, attributi = [];
-		for (var i = 0; i < document.getElementsByName('att').length; i++) {
-			attributi[i] = document.getElementsByName('att')[i].value;
+function modifica_modello(id_modello) {
+	var modello = {}, attributi = [];
+	for (var i = 0; i < document.getElementsByName('att').length; i++) {
+		attributi[i] = document.getElementsByName('att')[i].value;
+	}
+	modello = {
+		"id_modello" : parseInt(document.getElementById("id_modello").value),
+		"nome_modello" : document.getElementById("nome_modello").value,
+		"attributi" : attributi
+	};
+	$.ajax({
+		url : "http://localhost:8080/modello/modifica",
+		type: "PUT",
+		contentType: "application/json",
+		data: JSON.stringify(modello),
+		dataType: "json"
+	}).done(function (esito) {
+		if (esito) {
+			window.location.href = '../modello';
+		} else {
+			window.location.href = '../error';
 		}
-		modello = {
-			"id_modello" : parseInt(document.getElementById("id_modello").value),
-			"nome_modello" : document.getElementById("nome_modello").value,
-			"attributi" : attributi
-		};
-		$.ajax({
-			url : "http://localhost:8080/modello/modifica",
-			type: "PUT",
-			contentType: "application/json",
-			data: JSON.stringify(modello),
-			dataType: "json"
-		}).done(function (esito) {
-			if (esito) {
-				window.location("../modello");
-			} else {
-				window.location("../error");
-			}
-		});
 	});
 }
 
@@ -112,34 +111,43 @@ function elimina_modello(id_modello) {
 
 //UTILIZZATI NELLA PAGINA MODELLO
 function visualizzaModelliInPagina(username) {
-	try {
-		idModelliUtente(username).forEach(function(idModello) {
-			mostraModello(richiedi_modello(idModello));
-		});
-		
-	} catch(error) {
-		console.log(error);
-	}
+	idModelliUtente(username).forEach(function(id_modello) {
+		var modello, i, cont = 1, tabella = "";
+		modello = richiedi_modello(id_modello);
+		//HEAD DELLA TABELLA
+		tabella += "<div class=wrap-table100 style='width: 70%;margin: 0 0 2% 15%;'><div class='table100 ver1'>";
+		tabella += "<table id='" + id_modello + "'><thead class='table100-head'><tr class='row100 head'>";
+		tabella += "<th/><th/><th class='cell100 column1'>" + modello.nome_modello + "</th><th/><th/></tr></thead>";
+		//BODY DELLA TABELLA
+		tabella += "<tbody class='table100-body js-pscroll'><tr class='row100 body'>";
+		for (i = 0; i < modello.attributi.length; i++) {
+			if (cont > 5) {
+				tabella += "</tr><tr class='row100 body'>";
+				cont = 1;
+			}
+			tabella += "<td class='cell100 column" + (cont++) + "'>" + modello.attributi[i] + "</td>";
+		}
+		tabella += "</tr></tbody></table></div></div>";
+		$("#modelli").append("<div id='utilityButtons" + id_modello + "'></div>");
+		$("#utilityButtons" + id_modello).append("<button class=button type=submit style='clear:right;'>Crea Tabella</button>");
+		$("#utilityButtons" + id_modello).append("<button id='bttModify" + id_modello + "' class=button type=submit style='clear:right;' onClick=acquisizione_dati(" + id_modello + ");>Modifica modello</button>");
+		$("#utilityButtons" + id_modello).append("<button class=button type=submit style='clear:right;' onClick=elimina_modello(" + id_modello + ");>Elimina</button>");
+		$("#modelli").append(tabella);	
+	});
 }
 
-function mostraModello(modello) {
-	var i, cont = 1, tabella = "";
-	//HEAD DELLA TABELLA
-	tabella += "<div class='wrap-table100' style='width: 70%;margin: 0 0 2% 10%;'><div class='table100 ver1'>";
-	tabella += "<table><thead class='table100-head'><tr class='row100 head'>";
-	tabella += "<th/><th/><th class='cell100 column1'>" + modello.nome_modello + "</th><th/><th/></tr></thead>";
-	//BODY DELLA TABELLA
-	tabella += "<tbody class='table100-body js-pscroll'><tr class='row100 body'>";
-	for (i = 0; i < modello.attributi.length; i++) {
-		if (cont > 5) {
-			tabella += "</tr><tr class='row100 body'>";
-			cont = 1;
-		}
-		tabella += "<td class='cell100 column" + (cont++) + "'>" + modello.attributi[i] + "</td>";
-	}
-	tabella += "</tr></tbody></table></div></div>";
-	$("#modelli").append("<button class='button' type='submit'>Crea Tabella</button>");
-	$("#modelli").append("<button class='button' type='submit' style='clear:right; margin-top: 1.2%;' onClick='elimina_modello(" + modello.id_modello + ");'>Elimina</button>");
-	$("#modelli").append(tabella);
-	
+function acquisizione_dati(id_modello) {
+	var $headTable, $bodyTable, vecchioModello;
+	vecchioModello = richiedi_modello(id_modello);
+	//CERCO L'HEAD E IL BODY DELLA TABELLA CHE MI SERVE ATTRAVERSO IL SUO ID, CHE SARà UGUALE A QUELLO DEL MODELLO
+	$headTable = $("#" + id_modello + " thead");
+	$bodyTable = $("#" + id_modello + " tbody");
+	//MODIFICO LA TABELLA AFFFINCHE POSSA PRENDERE IN INPUT I DATI
+	$headTable.children().children(".cell100.column1").html("<input type='text' id='nuovoNome" + id_modello + "' value='" + vecchioModello.nome_modello + "' />");
+	$bodyTable.children().each(function() {
+		$(this).children().each(function () {
+			$(this).html("<input type=text class='nuoviAttributi" + id_modello + "' value=" + $(this).text() + " />");
+		});
+	});
+	$("#bttModify" + id_modello).replaceWith("<button class='button modify' type=submit style=''>Annulla</button><button class='button modify' type=submit style=''>Salva</button>");
 }
